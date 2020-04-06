40a3e5d59bfb1e1563c99388424a798df622bf9f
michel novellino <michelnovellino.programador@gmail.com>
2020-04-06 19:24:17 -0400

    sirviendo assets

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/framework', express.static(__dirname + '/framework'));
+app.use('/assets', express.static(__dirname + '/assets'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     console.log(__dirname )
